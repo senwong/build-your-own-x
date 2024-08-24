f2a1dc6b597e1528eca28b7f4387ef9ef0c4a699
senwong <wangshaosen666@gmail.com>
2024-08-24 08:22:41 +0800

    修改

--- diffieHellman.js
+++ diffieHellman.js
@@ -154,10 +154,13 @@ function hkdfExpandLabel(prk, label, context, length) {
  * @param {string} iv 
  * @param {string} data 
  */
-function decrypt(key, iv, data) {
+function decrypt(key, iv, authTag, recData, data) {
     
-  const decipher = createDecipheriv('aes-128-gcm', Buffer.from(key, "hex"), Buffer.from(iv, "hex"));
-  const decryptedData = decipher.update(data, 'hex', 'hex');
+  const decipher = createDecipheriv('aes-256-gcm', Buffer.from(key, "hex"), Buffer.from(iv, "hex"));
+  decipher.setAAD(recData, { encoding: "hex" });
+  decipher.setAuthTag(authTag, "hex");
+  
+  let decryptedData = decipher.update(data, 'hex', 'hex');
   decryptedData += decipher.final('hex');
   return decryptedData;
 }
@@ -198,4 +201,7 @@ console.log("🚀 ~ clientHandShakeIV:", clientHandShakeIV)
 console.log("🚀 ~ serverHandShakeIV:", serverHandShakeIV)
 
 
-const decryptData = decrypt(serverHandShakeKey, serverHandShakeIV, );
+const authTag = "9ddef56f2468b90adfa25101ab0344ae";
+const recData="1703030017";
+const decryptData = decrypt(serverHandShakeKey, serverHandShakeIV, authTag, recData, "6be02f9da7c2dc");
+console.log("🚀 ~ decryptData:", decryptData)
